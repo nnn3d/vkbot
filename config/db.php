bcf1b566c432f5f6aad7afeb76b0a96fa6c132d0
nnn3d <nnn3d@mail.ru>
2017-03-19 03:57:50 +0300

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=82.146.45.63:3306;dbname=vkbot',
-    'username' => 'user',
-    'password' => '1qaz@WSX',
-    'charset' => 'utf8',
-];
