d5b888b8aec4ff2e1addd99103e98ee387ee058c
goodwhife <thedmkw@yandex.ru>
2017-03-28 20:30:56 +0300

    add brak

--- config/constants.php
+++ config/constants.php
@@ -6,5 +6,6 @@ define('USER_STATUS_DEFAULT', 1);
 define('COMMAND_USER', 'user');
 define('COMMAND_BOT', 'bot');
 define('COMMAND_DUEL', 'duel');
+define('COMMAND_MARRIAGE', 'brak');
 
 define('CHAT_PARAMS_COMMAND_PREFIX', 'status command ');
