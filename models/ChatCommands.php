94ea49850125000534e90076057f3b258459af73
goodwhife <thedmkw@yandex.ru>
2017-03-30 01:41:49 +0300

    Update ChatCommands.php

--- models/ChatCommands.php
+++ models/ChatCommands.php
@@ -82,8 +82,9 @@ class ChatCommands
                     return false;
                 } else if ($command->getArgs()[1] == 'да') {
 		    $marriage = ChatParams::findOne(['param' => COMMAND_MARRIAGE, 'chatId' => $command->chatId]);
-		    if(!$marriage) ChatParams::setMarriage($command->chatId, COMMAND_MARRIAGE, '1');
-		    ChatParams::updateMarriage($command->chatId, COMMAND_MARRIAGE, '235');	
+		    $value = array($user1.','.$user2);
+		    if(!$marriage) ChatParams::setMarriage($command->chatId, COMMAND_MARRIAGE, $value);
+		    ChatParams::updateMarriage($command->chatId, COMMAND_MARRIAGE, $value);	
 		    $chat->sendMessage("{$user1->name} {$user1->secondName} и {$user2->name} {$user2->secondName} теперь женаты!");
 		    $brak->delete();
 		    return false;
