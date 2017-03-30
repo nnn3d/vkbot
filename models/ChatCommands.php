235d1fd0d37672cf44c85f5c214ec8b4e882fab2
goodwhife <thedmkw@yandex.ru>
2017-03-30 16:54:39 +0300

    Update ChatCommands.php

--- models/ChatCommands.php
+++ models/ChatCommands.php
@@ -91,7 +91,7 @@ class ChatCommands
 		    	    $value = $marriage->value;
 			    $value = unserialize($value);
 			    $strVal = array($user1->userId, $user2->userId, time());
-		    	    $value = array_unshift($value, $strVal);
+		    	    $value = array_push($value, $strVal);
 		    	    ChatParams::updateMarriage($command->chatId, COMMAND_MARRIAGE, $value);	
 		    }
 			
