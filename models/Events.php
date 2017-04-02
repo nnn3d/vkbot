3ebb5a127509dccc22ac5644c09a4b314f15f9e3
goodwhife <thedmkw@yandex.ru>
2017-04-02 18:10:35 +0300

    Update Events.php

--- models/Events.php
+++ models/Events.php
@@ -38,9 +38,38 @@ class Events extends \yii\db\ActiveRecord
         ];
     }
 
-    public static function setEvent($chatId, $userId, $time, $event)
+    public static function setEvent($chatId, $userId, $time, $event, $midEvent == null)
     {
         if (Events::find()->where(['chatId' => $chatId, 'userId' => $userId, 'time' => $time])->exists()) return false;
+        
+        switch ($event){ 
+            case "chat_create": 
+            $event = "chat_create";
+            break; 
+            case "chat_title_update": 
+            $event = "title_update";
+            break; 
+            case "chat_photo_update": 
+            $event = "photo_update";
+            break; 
+            case "chat_invite_user":
+            if($userId == $midEvent) {
+                $event = "return_user";
+            } else {
+                $event = "invite_user";
+            } 
+            break; 
+            case "chat_kick_user": 
+            if($userId == $midEvent) {
+                $event = "leave_user";
+            } else {
+                $event = "kick_user";
+            }
+            break; 
+            default:
+            break; 
+        }
+
         $self = new self([
             'chatId' => $chatId,
             'userId' => $userId,
