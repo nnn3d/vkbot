98fcdccef7e60ea55beb91704ee3a78e5100d778
goodwhife <thedmkw@yandex.ru>
2017-04-30 16:03:11 +0300

    Update params.php

--- config/params.php
+++ config/params.php
@@ -1,6 +1,6 @@
 <?php
 require_once(__DIR__ . '/constants.php');
-$botName = YII_TEST ? 'тест' : 'торч';
+$botName = YII_TEST ? 'тест' : 'Мия'; // основное имя. в помощи и подсказках будет использоваться это имя.
 return [
     'adminEmail' => 'admin@example.com',
     'vkBot' => [
