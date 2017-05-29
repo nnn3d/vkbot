7dc376ee3f5e765363e4a61eb1c6a0af28d0175b
Torch Niko <Torch Niko>
2017-05-29 23:26:53 +0300

    smiles bugfix

--- config/params.php
+++ config/params.php
@@ -9,7 +9,7 @@ return [
     		// 'scope' => '69634',
     		// 'secret_key' => 'z10qtaeMP0UoNGBobIxz',
     		'v' => '5.60',
-			'access_token' => '4a58dc628be40bc3b9d64d3f73d20b7e2dd4e437c2',
+			'access_token' => 'fdec60ac1f13e4e2454d8d39883f284ed4dc18e47ef3e37747bbd09d0387e1212b9c0429cfa54fb35f533',
     	],
         'statusLabels' => [
             USER_STATUS_ADMIN => 'админ',
