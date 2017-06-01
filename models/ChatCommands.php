bdd947345c6f1413de2903569af959be30ac20f2
penmet <banditkin40k@gmail.com>
2017-06-02 00:14:17 +0300

    12313

--- models/ChatCommands.php
+++ models/ChatCommands.php
@@ -1389,8 +1389,7 @@ class ChatCommands
             },
             function ($command) {
                 $chat                                      = Chats::getChat($command->chatId);
-                $chatName = ChatParams::get($command->chatId)->chatName;
-		$chatName->delete();
+                ChatParams::__set(($command->chatId)->chatName,'');
                 $chat->sendMessage("Название разблокировано!", ['forward_messages' => $command->messageId]);
             },
             ['statusDefault' => USER_STATUS_MODER]
