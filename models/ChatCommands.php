fe0b5bbf3b3370edf33a90f8698b18ed01e1ded1
penmet <banditkin40k@gmail.com>
2017-06-01 22:18:38 +0300

    123

--- models/ChatCommands.php
+++ models/ChatCommands.php
@@ -1163,8 +1163,8 @@ class ChatCommands
 			];
 			}
 		    	foreach($daystat as $num1 => $item1){
-				$n = $num1 + 1;
-				if ($n>1) {
+				$m = $num1 + 1;
+				if ($m>2) {
 				$item3=$item1[$num1 - 1];
 				$item1['fullstat'] = $item1['fullstat'] - $item3['fullstat'];
 				}
