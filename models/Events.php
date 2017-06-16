8c5b153a94d8eaf8ac777dffbbadc59e0a9bc0c4
penmet <banditkin40k@gmail.com>
2017-06-16 15:32:03 +0300

    untuchable rules + welcome

--- models/Events.php
+++ models/Events.php
@@ -148,6 +148,13 @@ class Events extends \yii\db\ActiveRecord
 		$chat->sendMessage("Правила конфы:\n {$rules}");
 		return false;
 	} else if(Users::getStatus($chatId, $userId) == USER_STATUS_UNTOUCHABLE) {
+		$rules = ChatParams::get($chat->chatId)->rules;
+		$welcome = ChatParams::get($chat->chatId)->welcome;
+		if (!$welcome) return false;
+		$chat->sendMessage("[id{$invitationUserId}|{$invitationUser->name} {$invitationUser->secondName}], {$welcome}");
+		if (!$rules) return false;
+		$chat->sendMessage("Правила конфы:\n {$rules}");
+		return false;
 		$user->status = USER_STATUS_DEFAULT;
 		$user->save();
 		$chat->sendMessage("{$user->name} {$user->secondName} использовал свое право пригласить человека.\n\nЯ уже изменила его статус.");
