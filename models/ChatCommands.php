59ba0b83b1116b796972a04edb7d8817d92e74b6
penmet <banditkin40k@gmail.com>
2017-07-07 15:05:37 +0300

    1asdasdas

--- models/ChatCommands.php
+++ models/ChatCommands.php
@@ -1350,7 +1350,7 @@ class ChatCommands
                     $user = Users::getUser($command->chatId, $id);
                 } else {
 		    $chat->sendMessage("ЩОРСУНЧИК {$kickuser}");
-                    $usercall=explode(" ", implode($kickmass));
+                    $usercall=explode(" ", implode($kickuser));
                     $name       = trim($usercall[0]);
                     $secondName = isset($usercall[1]) ? trim($usercall[1]) : '';
                     $user       = Users::getUserByName($command->chatId, $name, $secondName);
