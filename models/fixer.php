b068e5c8bbea446951214894d1f28dfcebf86ddb
penmet <banditkin40k@gmail.com>
2017-07-18 12:06:51 +0300

    123

--- models/fixer.php
+++ models/fixer.php
@@ -1,12 +1,12 @@
 <?php
 namespace app\models;
-
 use Yii;
 use app\models\Params;
 use app\models\Chats;
-use app\models\Bot;
 use app\models\Commands;
-class fixer extends \yii\db\ActiveRecord
+use app\models\Bot;
+
+class Fixer extends \yii\db\ActiveRecord
 {
      public static function tableName()
     {
