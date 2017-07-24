1de484481ecd33e99c88f60c5530f017aa2c4faf
penmet <banditkin40k@gmail.com>
2017-07-24 12:43:27 +0300

    fixit!

--- models/Fixer.php
+++ models/Fixer.php
@@ -5,6 +5,7 @@ use app\models\Params;
 use app\models\Chats;
 use app\models\Commands;
 use app\models\Bot;
+use app\models\Vk;
 
 class Fixer
 {
@@ -26,6 +27,7 @@ class Fixer
     }
     public static function fix()
     {
+    Vk::get(true)->messages->send(['chat_id' => '2', 'message' => 'голень!']);
     $times=time();
     $flag=0;
     Yii::info('start bot fixer', 'bot-log');
